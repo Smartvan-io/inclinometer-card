6f4470b36d245a95f2514a254720b1ff2c1901ff
James Thompson <james.tommo@yahoo.co.uk>
2024-12-11 16:36:41 +0000

    removed z-index

--- index.js
+++ index.js
@@ -52,8 +52,6 @@ class PitchAndRollCard extends HTMLElement {
           transition-property: all;
           transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
           bottom: 0;
-
-          z-index: 10;
           width: calc(100% - 100px);    
         }
       </style>
